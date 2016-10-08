2ad1a26e9725d86017f2a39f0427710503ac3eb7
ICELI <woticeli@gmail.com>
2016-10-08 19:01:42 +0800

    add less-loader demo

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,8 +1,9 @@
-var app = require('./app.js')
-require('../css/bootstrap/css/bootstrap.min.css')
-require('../css/style.css')
-require('../css/icon.css')
-require('../js/directive/directive.js')
-require('../js/service/service.js')
-require('../js/controller/publicCtrl.js')
-// require('controller/homeCtrl')
+var app = require('./app.js');
+require('../css/less-demo.less');
+require('../css/bootstrap/css/bootstrap.min.css');
+require('../css/style.css');
+require('../css/icon.css');
+require('../js/directive/directive.js');
+require('../js/service/service.js');
+require('../js/controller/publicCtrl.js');
+// require('controller/homeCtrl');
