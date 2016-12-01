e5ba3b32f2b7e9a17a91087279d959abda3003f1
ICELI <woticeli@gmail.com>
2016-12-01 23:37:26 +0800

    remove app.js

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,13 +1,12 @@
-var app = require('./app.js');
 require('../css/bootstrap/css/bootstrap.min.css');
 require('../dep/ng-lazy-image/lazy-image-style.css');
 require('../css/style.css');
 require('../css/postcss-demo.css');
 require('../css/less-demo.less');
 require('../css/sass-demo.scss');
+
 require('../js/directive/directive.js');
 require('../js/filter/filter.js');
 require('../js/service/service.js');
 require('../js/service/dialogs.js');
 require('../js/controller/publicCtrl.js');
-// require('controller/homeCtrl');
