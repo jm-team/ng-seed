8bb865dedfd41e8e28b8c9b0dbde7a008e005908
ICELI <woticeli@gmail.com>
2017-03-02 15:39:57 +0800

    fix: set Status Code 404 to the crawler

--- src/js/router/error/404.js
+++ src/js/router/error/404.js
@@ -3,7 +3,17 @@ var templateUrl = require('page/error/404.html');
 module.exports = {
     url: '/404',
     templateUrl: templateUrl,
-    controller: 'HomeCtrl',
+    controller: function () {
+        setPrerenderStatusCode(404);
+
+        // 解决爬虫爬取404页面 statusCode 200问题 https://prerender.io/documentation/best-practices
+        function setPrerenderStatusCode(statusCode) {
+            var meta = document.createElement('meta');
+            meta.name = "prerender-status-code";
+            meta.content = statusCode || "404";
+            document.getElementsByTagName('head')[0].appendChild(meta);
+        }
+    },
     data: {
         displayName: ''
     }
