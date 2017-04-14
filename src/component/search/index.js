b5aa53675bd636225a4930a78e7143975a845ad4
ICELI <woticeli@gmail.com>
2017-04-14 15:19:43 +0800

    fix: 获取跳转之后的url

--- src/component/search/index.js
+++ src/component/search/index.js
@@ -9,7 +9,7 @@ angular.module('jmui.search', [])
             $timeout(function () {
                 var originRouter = $state.current.name;
 
-                scope.$on('$locationChangeStart', function (obj, newUrl, oldUrl) {
+                scope.$on('$locationChangeSuccess', function (obj, newUrl, oldUrl) {
                     // fix 离开当前路由，防止不必要查询
                     var currentRouter = $state.current.name;
 
