9fa49c4e9afc511b650071c95e6165cb338b5477
myzhoulang <604389771@qq.com>
2017-04-18 18:31:03 +0800

    style:  一部分编辑器格式化后的代码

--- src/controller/homeCtrl.js
+++ src/controller/homeCtrl.js
@@ -1,44 +1,42 @@
-;(function () {
-    var app = require('app');
+var app = require('app');
 
-    // 调用Api 服务
-    app.registerController('HomeCtrl', HomeCtrl);
+// `Home` 控制器
+app.registerController('HomeCtrl', HomeCtrl);
 
-    ///////////////////////////
+///////////////////////////
 
-    /*@ngInject*/
-    function HomeCtrl(Api, $modal, $state) {
-        var vm = this;
+/*@ngInject*/
+function HomeCtrl(Api, $modal, $state) {
+  var vm = this;
 
-        vm.title = "Home Page";
-        vm.desc = "这是主页";
-        vm.data = [1, 2, 3, 4, 5];
-        vm.collection = [];
-        vm.searchText = 1;
-        vm.changeImageUrl = changeImageUrl;
-        vm.changeImage = changeImage;
+  vm.title = "Home Page";
+  vm.desc = "这是主页";
+  vm.data = [1, 2, 3, 4, 5];
+  vm.collection = [];
+  vm.searchText = 1;
+  vm.changeImageUrl = changeImageUrl;
+  vm.changeImage = changeImage;
 
-        vm.changeImage();
+  vm.changeImage();
 
-        ///////////////////////////////////
+  ///////////////////////////////////
 
-        // lazy img
-        function changeImageUrl() {
-            return '//placehold.it/768x599/' + Math.floor(Math.random() * 16777215).toString(16) + '/ffffff';
-        }
+  // lazy img
+  function changeImageUrl() {
+    return '//placehold.it/768x599/' + Math.floor(Math.random() * 16777215).toString(16) + '/ffffff';
+  }
 
-        /* build random item list */
-        for (var i = 0; i < 20; i++) {
-            vm.collection.push({
-                type: Math.floor(Math.random() * 2) + 1,
-                number: i + 1,
-                srcset: changeImageUrl()
-            });
-        }
+  /* build random item list */
+  for (var i = 0; i < 20; i++) {
+    vm.collection.push({
+      type: Math.floor(Math.random() * 2) + 1,
+      number: i + 1,
+      srcset: changeImageUrl()
+    });
+  }
 
-        function changeImage() {
-            vm.runtimeImageSrc = changeImageUrl();
-        }
+  function changeImage() {
+    vm.runtimeImageSrc = changeImageUrl();
+  }
 
-    }
-})()
+}
