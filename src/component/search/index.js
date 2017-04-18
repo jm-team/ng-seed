2b47e0c1302facac947ec277a5bf8553af7ff1db
ICELI <woticeli@gmail.com>
2017-04-18 09:51:54 +0800

    revert: jmSearch

--- src/component/search/index.js
+++ src/component/search/index.js
@@ -1,33 +1,20 @@
-/*global angular*/
-(function () {
-  "use strict";
-
-  angular.module('jmui.search', [])
+angular.module('jmui.search', [])
     .factory('jmSearch', function ($state, $timeout) {
 
-      return saveSearchUrl;
+        return saveSearchUrl;
 
-<<<<<<< HEAD
+        function saveSearchUrl(scope, callback) {
+            // fix 延迟注册，防止切换回当前路由触发2次查询
+            angular.isFunction(callback) &&
+            $timeout(function () {
+                var originRouter = $state.current.name;
+
                 scope.$on('$locationChangeSuccess', function (obj, newUrl, oldUrl) {
                     // fix 离开当前路由，防止不必要查询
                     var currentRouter = $state.current.name;
-=======
-      function saveSearchUrl(scope, callback) {
-        // fix 延迟注册，防止切换回当前路由触发2次查询
-        if (angular.isFunction(callback)) {
-          $timeout(function () {
-            var originRouter = $state.current.name;
->>>>>>> ui-branch
 
-            scope.$on('$locationChangeStart', function (obj, newUrl, oldUrl) {
-              // fix 离开当前路由，防止不必要查询
-              var currentRouter = $state.current.name;
-              if (originRouter === currentRouter) {
-                callback();
-              }
+                    originRouter === currentRouter && callback();
+                });
             });
-          }, 0);
         }
-      }
     });
-}());
