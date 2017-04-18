5d560cc69a7fa58236bbc675c1a0a644f21db5f0
ICELI <woticeli@gmail.com>
2017-04-18 09:58:52 +0800

    revert: merge

--- src/component/jmui.js
+++ src/component/jmui.js
@@ -7,39 +7,30 @@ require('./tab');
 require('./scroll');
 require('./imgerror');
 require('./search');
-<<<<<<< HEAD
 require('./backtop');
 require('./dialog');
 require('./loadScript');
 require('./ueditor/angular-ueditor');
-=======
-require('./ueditor/angular-ueditor');
 
-require('./backtop');
-require('./dialog');
 require('./switch');
 require('./alert');
 require('./rate');
 
->>>>>>> ui-branch
-
 angular.module('jmui', [
     'jmui.accordion',
     'jmui.crumbs',
-    'jmui.switch',
-    'jmui.alert',
-    // 'jmui.login',
-
     'jmui.tooltip',
     'jmui.login',
     'jmui.pagination',
     'jmui.tab',
     'jmui.scroll',
-    'jmui.rate',
     'jmui.imgerror',
     'jmui.search',
     'jmui.AnchorSmoothScroll',
     'jmui.dialog',
     'jmui.loadScript',
+    'jmui.switch',
+    'jmui.alert',
+    'jmui.rate',
     'ng.ueditor'
 ]).value('version', '0.1');
