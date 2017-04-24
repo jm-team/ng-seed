52725022787e87f4191b44971bce84da93ef0ec3
myzhoulang <604389771@qq.com>
2017-04-24 11:26:46 +0800

    feat:  面包屑页面添加功能

--- src/page/components/crumb/crumb.controller.js
+++ src/page/components/crumb/crumb.controller.js
@@ -3,5 +3,10 @@ var app = require('app');
 app.registerController('CrumbCtrl', CrumbCtrl);
 
 function CrumbCtrl() {
+    var vm = this;
 
+    vm.indexCrumb = {
+        displayName:"首页",
+        router:"about"
+    };
 }
