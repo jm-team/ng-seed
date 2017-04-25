838b321c46beb3498772616baa04e947694ddb88
myzhoulang <604389771@qq.com>
2017-04-25 17:13:42 +0800

    fix:  修改components 路由中 没有写 ngInject

--- src/page/components/components.router.js
+++ src/page/components/components.router.js
@@ -2,29 +2,30 @@ var templateUrl = require('./components.html');
 
 module.exports = {
     title: "组件",
-    url:"/components",
+    url: "/components",
     data: {
-    breadcrumbProxy: 'components',
-    displayName: '组件'
-  },
-  controller: function($scope){
-
-    $scope.indexCrumb = {
-      displayName:"首页",
-      router:"about"
-    }
-  },
-  templateUrl: templateUrl,
-  // controller: 'HomeCtrl',
-  // controllerAs: 'vm',
-  resolve: {
+        breadcrumbProxy: 'components',
+        displayName: '组件'
+    },
     /*@ngInject*/
-    loadCtrl: function ($q) {
-      // var defer = $q.defer();
-      // require.ensure([], function (require) {
-      //   defer.resolve(require('./../home/home.controller.js'));
-      // }, 'home');
-      // return defer.promise;
+    controller: function ($scope) {
+
+        $scope.indexCrumb = {
+            displayName: "首页",
+            router: "about"
+        }
+    },
+    templateUrl: templateUrl,
+    // controller: 'HomeCtrl',
+    // controllerAs: 'vm',
+    resolve: {
+        /*@ngInject*/
+        loadCtrl: function ($q) {
+            // var defer = $q.defer();
+            // require.ensure([], function (require) {
+            //   defer.resolve(require('./../home/home.controller.js'));
+            // }, 'home');
+            // return defer.promise;
+        }
     }
-  }
 };
