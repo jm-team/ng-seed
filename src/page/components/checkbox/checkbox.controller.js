0370825fb8dd0cfc345d722c7594a559223b84a1
myzhoulang <604389771@qq.com>
2017-04-25 15:18:15 +0800

    feat:  逐步添加checkbox 组件功能

--- src/page/components/checkbox/checkbox.controller.js
+++ src/page/components/checkbox/checkbox.controller.js
@@ -19,9 +19,43 @@ app.registerController('CheckboxCtrl',
             vm.checked = !vm.checked;
         };
 
-        vm.options = [
-            { label: 'Apple', value: 'Apple' },
-            { label: 'Pear', value: 'Pear' },
-            { label: 'Orange', value: 'Orange' }
-        ]
+        vm.options = ['Apple', 'Pear', 'Orange'];
+
+        vm.checkeds = ['Apple']
+
+        vm.options2 = [
+            { label: 'HTML', value: 'HTML' },
+            { label: 'CSS', value: 'CSS' },
+            { label: 'JavaScript', value: 'JavaScript' }
+        ];
+
+        vm.checkeds2 = ['JavaScript', 'CSS']
+
+        vm.options3 = [
+            { label: 'Angular', value: 'Angular' },
+            { label: 'Vue', value: 'Vue',disabled: true },
+            { label: 'React', value: 'React', disabled: true }
+        ];
+
+        vm.checkeds3 = ['React', 'Angular']
+
+
+        // 全选
+        vm.checkAll = false;
+        vm.options4 = [
+            { label: 'Angular', value: 'Angular' },
+            { label: 'Vue', value: 'Vue'},
+            { label: 'React', value: 'React'},
+            { label: 'Backbone', value: 'Backbone', disabled: true},
+            { label: 'Knockout', value: 'Knockout',  disabled: true}
+        ];
+        vm.checkeds4 = ['React', 'Angular']
+
+        vm.onChange = function(){
+            var l = vm.options4.filter(function(item){
+                return !item.disabled
+            }).length;
+
+            vm.checkAll = (l === vm.checkeds4.length)
+        };
     });
