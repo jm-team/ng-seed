aabe26e93fcef19ccd74d3284427f92049430044
ICELI <woticeli@gmail.com>
2017-04-28 11:33:56 +0800

    fix: remove es6, conflict chunks name

--- src/page/components/autocomponent/autocomponent.controller.js
+++ src/page/components/autocomponent/autocomponent.controller.js
@@ -13,27 +13,29 @@ function AutoComponentCtrl($q) {
     vm.results = [];
     vm.value = null;
 
-    vm.handleChange = function(){
+    vm.handleChange = function () {
         return searchResult(vm.keyword);
     };
 
-    vm.select = function(arg){
+    vm.select = function (arg) {
         console.log(arg);
         vm.value = arg.item;
     }
 
-
-    function getRandomInt(max, min = 0) {
-        return Math.floor(Math.random() * (max - min + 1)) + min; // eslint-disable-line no-mixed-operators
+    function getRandomInt(max, min) {
+        min = min || 0;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     function searchResult(query) {
         return $q.when((new Array(getRandomInt(5))).join('.').split('.')
-            .map((item, idx) => ({
-                query,
-                category: `${query}${idx}`,
-                count: getRandomInt(200, 100),
-            })))
+            .map(function (item, idx) {
+                return {
+                    query: query,
+                    category: query + idx,
+                    count: getRandomInt(200, 100),
+                }
+            }))
 
     }
 }
