050b1e41d5b87e0c7971566232e6f8af7710ca6d
ICELI <woticeli@gmail.com>
2017-05-22 13:56:31 +0800

    chore: Comments for webpack production config

--- build/webpack-production.js
+++ build/webpack-production.js
@@ -24,12 +24,12 @@ module.exports = {
 
     postcss: function () {
         return [
-            // 合并雪碧图
+            // 合并雪碧图插件
             require('postcss-sprites')({
                 stylesheetPath: './src/asset/css',
                 spritePath: './dist/img/sprite',
 
-                // 添加雪碧图规则 只有在sprite文件夹下的图片进行合并
+                // 添加雪碧图规则 只有在sprite文件夹下的图片才进行合并
                 filterBy: function (image) {
                     if (!/\/sprite\//.test(image.url)) {
                         // console.log(image.url);
@@ -39,7 +39,7 @@ module.exports = {
                     return Promise.resolve();
                 },
 
-                // 添加雪碧图规则 在sprite下，如果包含文件夹则单独进行合并
+                // 添加雪碧图规则 在sprite文件夹下，如果包含子文件夹则该文件夹包含的图片单独进行合并
                 groupBy: function (image) {
                     var regex = /\/sprite\/([^/]+)\//g;
                     var m = regex.exec(image.url);
@@ -49,12 +49,12 @@ module.exports = {
                     return Promise.resolve(m[1]); // 'sprite.' + icon + '.png'
                 },
 
-                // 图片之间的间距
+                // 图片之间的距离
                 spritesmith: {
                     padding: 20
                 }
             }),
-            // css浏览器兼容性补全
+            // css3的浏览器兼容性补全插件
             require('autoprefixer')({
                 browsers: ["last 6 version"]
             })
@@ -69,14 +69,14 @@ module.exports = {
         new ngAnnotatePlugin({
             add: true
         }),
-        // 将webpack runtime从vendor里抽出
+        // 将webpack runtime 和 manifest 从vendor里抽出
         new webpack.optimize.CommonsChunkPlugin({
             name: ["vendor", "manifest"], // vendor libs + extracted manifest
             minChunks: Infinity
         }),
-        // 根据依赖次数进行模块排序
+        // 根据模块依赖次数进行排序
         new webpack.optimize.OccurenceOrderPlugin(),
-        // 将模块Id转为hash值，解决模块id为数字时不稳定的问题，对应开放环境HashedModuleIdsPlugin
+        // 将模块Id转为hash值，解决模块id为数字时不稳定的问题，对应开发环境HashedModuleIdsPlugin
         new HashedModuleIdsPlugin(),
         // 将webpack runtime内嵌至index.html，需要和HtmlWebpackPlugin配合使用
         // 需在模板文件加上占位符 <%=htmlWebpackPlugin.files.webpackManifest%>
