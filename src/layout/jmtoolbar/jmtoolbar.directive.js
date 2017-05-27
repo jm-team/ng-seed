fc69952d017cc4ddfb63d48a8ad8309cb02b9e9c
ICELI <woticeli@gmail.com>
2017-05-27 11:32:20 +0800

    refactor: jmToolbar remove unused

--- src/layout/jmtoolbar/jmtoolbar.directive.js
+++ src/layout/jmtoolbar/jmtoolbar.directive.js
@@ -2,48 +2,14 @@ var app = require('app');
 var jmToolbar = require('./jmtoolbar.html');
 require('./jmtoolbar.scss');
 
-app.directive('jmToolbar', function ($window) {
+app.directive('jmToolbar', function () {
     return {
         restrict: 'AE',
         replace: true,
         templateUrl: jmToolbar,
         link: function(scope, element, attrs){
             var $elem = angular.element(element);
-            var winWidth,winHeight,rightWidth,bottomH;
-            function getSize(){
 
-                if (window.innerWidth)
-                    winWidth = window.innerWidth;
-                else if ((document.body) && (document.body.clientWidth))
-                    winWidth = document.body.clientWidth;
-                if(window.innerHeight)
-                    winHeight = window.innerHeight;
-                else if(document.body&&document.body.clientHeight)
-                    winHeight = document.body.clientHeight;
-            }
-            function setPos(){
-                (winWidth-1190)/2-58>0?rightWidth=(winWidth-1190)/2-58:rightWidth=0;
-                //winHeight>350?bottomH=(winHeight-350)/2:bottomH = 0;
-                $elem.css('position','fixed');
-                $elem.css('right',rightWidth+'px');
-                // $elem.css('right','25px');
-                //$elem.css('bottom',bottomH+'px');
-                $elem.css('bottom','80px');
-            }
-
-            scope.onResize = function(){
-                getSize();
-                setPos();
-            }
-            angular.element($window).bind('resize', function() {
-                scope.onResize();
-            })
-            getSize();
-            setPos();
-            // $elem.find('h3').on('click',function(){
-            //     $elem.toggleClass('on');
-            //     $elem.css('bottom',bottomH+'px');
-            // })
             $elem.find('li').eq(9).on('click',function(){
                 window.scrollTo(0,0);
             })
