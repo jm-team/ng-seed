e72c4e2e2080726baf12c9f2227c51a64a74e478
myzhoulang <604389771@qq.com>
2017-06-05 11:02:06 +0800

    refactor: 修改默认图片 require 方式

--- src/page/components/lazyimg/lazyimg.controller.js
+++ src/page/components/lazyimg/lazyimg.controller.js
@@ -8,7 +8,7 @@ var app = require('app');
 app.registerController('LazyimgCtrl', LazyimgCtrl);
 
 /*@ngInject*/
-function LazyimgCtrl() {
+function LazyimgCtrl(Static) {
   var vm = this;
 
   vm.data = [1, 2, 3, 4, 5];
@@ -39,7 +39,7 @@ function LazyimgCtrl() {
     vm.runtimeImageSrc = changeImageUrl();
   }
 
-  vm.defaultImage = require("../../../asset/img/logo.png");
+  vm.defaultImage = Static.defaultImage;//require("../../../asset/img/logo.png");
 
 }
 
