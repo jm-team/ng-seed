117975caae5dccf3ebbebe67b32619ac8c549c5d
ICELI <woticeli@gmail.com>
2017-07-04 22:23:31 +0800

    fix: dll git add with path
    - cd path/file is not work

--- build/webpack.dll.conf.js
+++ build/webpack.dll.conf.js
@@ -65,7 +65,7 @@ module.exports = {
         // 将新增的dll文件提交至git
         new WebpackOnBuildPlugin(function(stats) {
             // Do whatever you want...
-            exec('cd ./dep/vendor && git add -A')
+            exec('git add dep/vendor -A')
         })
     ]
 }
